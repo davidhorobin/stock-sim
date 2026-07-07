92a1df819bb63008f3f002a9e596f5da344a65e5
David Horobin <dwghorobin@gmail.com>
2026-07-07 22:56:19 +0100

    From templates

--- src/homepage.py
+++ src/homepage.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def homepage():
-    return "<p>Hello World!</p>"
+    return render_template("homepage.html")
