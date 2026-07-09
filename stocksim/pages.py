a523a35371681f1f3088f6d911743a51e08067e1
David Horobin <dwghorobin@gmail.com>
2026-07-09 15:38:33 +0100

    Functional stock lookup page

--- stocksim/pages.py
+++ stocksim/pages.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from . import queries
+import yfinance as yf
 
 bp = Blueprint('pages', __name__)
 
@@ -8,3 +9,27 @@ bp = Blueprint('pages', __name__)
 def index():
     top = queries.get_top_stocks(5)
     return render_template('pages/index.html', top_stocks=top)
+
+
+@bp.route('/stockinfo/', methods=['GET', 'POST'])
+@bp.route('/stockinfo/<symbol>', methods=['GET', 'POST'])
+def stockinfo(symbol=None):
+    if request.method == 'POST':
+        symbol = request.form['symbol']
+        error = None
+
+        if symbol == '':
+            error = "Please enter a stock symbol"
+
+        if error is None:
+            return redirect(url_for("pages.stockinfo", symbol=symbol))
+
+        flash(error)
+    if symbol is None:
+        info = None
+    else:
+        info = yf.Ticker(symbol).info
+        if len(info.keys()) <= 1 or info["quoteType"] != "EQUITY":
+            flash("Invalid stock symbol")
+            info = None
+    return render_template('pages/stockinfo.html', info=info)
