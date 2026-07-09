2b280729fd7d5c03f15cdd2ad69b99fd918f3111
David Horobin <dwghorobin@gmail.com>
2026-07-09 14:19:29 +0100

    Functional homepage and auth

--- stocksim/pages.py
+++ stocksim/pages.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from . import queries
 
 bp = Blueprint('pages', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('pages/index.html')
+    top = queries.get_top_stocks(5)
+    return render_template('pages/index.html', top_stocks=top)
