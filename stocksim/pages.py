1d73201f2172ed1be2711b10bd08a76131346ba4
David Horobin <dwghorobin@gmail.com>
2026-07-10 15:31:16 +0100

    Working newsfeed

--- stocksim/pages.py
+++ stocksim/pages.py
@@ -1,14 +1,26 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from . import queries
 import yfinance as yf
+from urllib.request import urlopen
+import re
+from datetime import *
 
 bp = Blueprint('pages', __name__)
 
 
 @bp.route('/')
 def index():
-    top = queries.get_top_stocks(5)
-    return render_template('pages/index.html', top_stocks=top)
+    url = "https://seekingalpha.com/market_currents.xml"
+    xml = urlopen(url).read().decode('utf-8')
+    articles = re.findall(r"<title>.*</title>|<link>.*</link>|<pubDate>.*</pubDate>", xml)
+    top_stories = []
+    for i in range(5):
+        article_time = datetime.strptime(strip_tags(articles[3 * i + 4]), "%a, %d %b %Y %H:%M:%S %z")
+        difference = (datetime.utcnow() - article_time.astimezone(timezone.utc).replace(tzinfo=None)).seconds // 60
+        top_stories += [
+            (strip_tags(articles[3 * i + 2]), strip_tags(articles[3 * i + 3]), difference)]
+    top_stocks = queries.get_top_stocks(5)
+    return render_template('pages/index.html', top_stocks=top_stocks, articles=top_stories)
 
 
 @bp.route('/stockinfo/', methods=['GET', 'POST'])
@@ -34,3 +46,7 @@ def stockinfo(symbol=None):
             flash("Invalid stock symbol")
             info = None
     return render_template('pages/stockinfo.html', symbol=symbol, info=info)
+
+
+def strip_tags(s):
+    return re.sub(r'<.*?>', '', s)
