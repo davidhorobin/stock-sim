0eaa59b58da7b7dd97ca6f29fa0add54c9b3dbac
David Horobin <dwghorobin@gmail.com>
2026-07-15 15:57:13 +0100

    Added test for login requirement

--- tests/test_trading.py
+++ tests/test_trading.py
@@ -2,11 +2,18 @@ import pytest
 from stocksim.db import get_db
 
 
-def test_portfolio(client, auth):
-    response = client.get('/portfolio')
+@pytest.mark.parametrize('path', (
+        '/portfolio',
+        '/buy/',
+        '/sell'
+))
+def test_login_required(client, path):
+    response = client.get(path)
     assert response.status_code == 302
     assert response.headers['Location'] == '/auth/login'
 
+
+def test_portfolio(client, auth):
     auth.login()
     response = client.get('/portfolio')
     assert response.status_code == 200
@@ -15,11 +22,7 @@ def test_portfolio(client, auth):
 
 
 def test_buy(client, auth):
-    response = client.get('/buy')
-    assert response.status_code == 302
-    assert response.headers['Location'] == '/auth/login'
-
     auth.login()
-    response = client.get('/buy')
+    response = client.get('/buy/')
     assert response.status_code == 200
     assert b"Buy Stocks" in response.data
