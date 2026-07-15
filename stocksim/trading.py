5ea6dd5c8b176752e970b3932415560770650207
David Horobin <dwghorobin@gmail.com>
2026-07-15 15:42:17 +0100

    reorganised

--- stocksim/trading.py
+++ stocksim/trading.py
@@ -4,6 +4,7 @@ from flask import (
 from stocksim.auth import login_required
 from stocksim.db import get_db
 from .queries import get_stock
+from .utils import calculate_profit
 
 bp = Blueprint('trading', __name__)
 
@@ -34,35 +35,6 @@ def portfolio():
         asset_total += tmp["total"]
     return render_template('trading/portfolio.html', profit=profit_total, assettotal=asset_total, balance=cash,
                            assets=sorted(asset_rows, key=lambda x: x['total'], reverse=True))
-
-
-def calculate_profit(db, symbol, user_id):
-    running_shares = 0
-    running_cost = 0
-    realised_profit_loss = 0
-    unrealised_profit_loss = 0
-
-    transactions = db.execute('SELECT * FROM ledger WHERE symbol=? AND user_id=? ORDER BY created',
-                              (symbol, user_id)).fetchall()
-    for t in transactions:
-        if t['type'] == "buy":
-            running_shares += t['shares']
-            running_cost += t['price'] * t['shares']
-        else:
-            avg_cost = running_cost / running_shares
-            sell_price = t['shares'] * t['price']
-            cost_basis = avg_cost * t['shares']
-            running_shares -= t['shares']
-            realised_profit_loss += sell_price - cost_basis
-            running_cost -= cost_basis
-
-    if running_shares > 0:
-        avg_cost = running_cost / running_shares
-        unrealised_profit_loss = (get_stock(symbol)["regularMarketPrice"] - avg_cost) * running_shares
-    else:
-        unrealised_profit_loss = 0
-
-    return (realised_profit_loss + unrealised_profit_loss, running_cost)
 
 
 @bp.route('/buy', methods=['GET', 'POST'])
